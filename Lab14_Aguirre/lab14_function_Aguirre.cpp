2d7398a5309dd783ba7add315e7d0f752c918739
DavidAguirre12 <david.aguirreduran56@student.qcc.cuny.edu>
2026-03-18 15:46:34 +0000

    Lab14_Aguirre

---
+++ Lab14_Aguirre/lab14_function_Aguirre.cpp
@@ -0,0 +1,99 @@
+/*
+David Aguirre
+Lab14, functions
+March 18, 2026
+*/
+
+#include <iostream>
+using namespace std;
+
+//Example 1: function calling function
+int multiply(int a, int b){
+    return a*b;
+}
+
+int calculatedb(int x, int y){
+    //function calling function
+    return 2*multiply(x,y);
+}
+
+//Example 2: recursive function
+void cheers(int n){
+    //base case
+    if(n==1){
+        cout<<"STOP"<<endl;
+    }
+    //recursive case
+    else{
+        cout<<(n*2)<<endl;
+        cheers(n-1);
+    }
+}
+
+/*TABLE OF ANALYSIS 
+Function     |             |                | else
+Iteration    |    int n    |    if(n==1)    |    cout<<(n*2)    |    cheer(n-1)    |
+------------------------------------------------------------------------------------
+    1        |      4      |     false      |       4*2=8       |    cheer(4-1=3)  |
+------------------------------------------------------------------------------------
+    2        |      3      |     false      |       3*2=6       |    cheer(3-1=2)  |
+------------------------------------------------------------------------------------
+    3        |      2      |     false      |       2*2=6       |    cheer(2-1=1)  |
+------------------------------------------------------------------------------------
+    4        |      1      |     true ---> cout<<"STOP"
+
+*/
+
+//Example 3: linear recursive function
+int linearfunction(int m){
+    if(m>10){
+        return -6;
+    }
+    else{
+        return(linearfunction(m+2) * (m-4));
+    }
+}
+
+/*TABLE OF ANALYSIS
+Function
+iteration    |    int m    |    if(m>10)    |        return(linearfunction(m+2) * (m*4));        |
+    1        |    int=3    |      False     |        return(linearfunction(3+2=5) * (3-4=-1))    |
+    2        |    int=5    |      False     |      return(linearfunction(5+2=7) * (5-4=1)*-1)    |
+    3        |    int=7    |      False     |     return(linearfunction(7+2=9) * (7-4=3)*-1*1)   |
+    4        |    int=9    |      False     |     return(linearfunction(9+2=11) * (9-4=5)*-1*1-3)|
+    5        |    int=11   |      True  ---> return -6
+
+    Final result= -6*5*3*1*-1
+    Final reuslt= 90
+*/
+
+
+//Example 4: Built-in functions
+//Program to calculate the hypotenuse of a right triangle
+//Function callects the number and returns the number if it is positive
+int collectnumber(){
+    int n;
+    cout<<"Enter a positive number: ";
+    cin>>n;
+
+    //recursive function to validate if n is positive
+    if (n<=0){
+        cout<<"Error! ";
+        return collectnumber();
+    }
+    else{
+        return n;
+    }
+}
+
+#include<cmath>
+
+//calculate and return the hypotenuse
+float hypotenuse(int side1, int side2){
+    return sqrt(pow(side1,2)+pow(side2,2));
+}
+
+//Print the result
+void printresult(int side1, int side2, float h){
+    cout<<"The hypotenuse of triangle with sides "<<side1<<" and "<<side2<<" is = "<<h<<endl;
+}
---
+++ Lab14_Aguirre/lab14_main_Aguirre.cpp
@@ -0,0 +1,31 @@
+/*
+David Aguirre
+Lab14, functions
+March 18, 2026
+*/
+
+#include <iostream>
+using namespace std;
+#include "lab14_function_Aguirre.cpp"
+
+int main(){
+
+    cout<<"----------Example 1: function calling function----------"<<endl;
+    int calculate = calculatedb(2,3);
+    cout<<calculate<<endl;
+    
+    cout<<"-----------Example 2: inclusive function-----------"<<endl;
+    cheers(4);
+
+    cout<<"----------Example 3: linear recursive -----------"<<endl;
+    int n = linearfunction(3);
+    cout<<"The Final result is: "<<n<<endl;
+
+    cout<<"----------Example 4: Built-in function----------"<<endl;
+    int side1 = collectnumber();
+    int side2 = collectnumber();
+    float h = hypotenuse(side1,side2);
+    printresult(side1, side2, h);
+
+    return 0;
+}
